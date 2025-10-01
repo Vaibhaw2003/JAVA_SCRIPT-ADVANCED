01f2020f75344b98ef0aa1bc3149f43460e9ac64
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2025-10-01 16:51:11 +0530

    done

--- DAY_01/script.js
+++ DAY_01/script.js
@@ -1,1 +1,152 @@
-alert("jay sri ram")
+//               variables-----
+
+// const rajput = "harsh singh";
+// var age = 21;
+// console.log(rajput + " "+ age)
+
+
+// var a =[1,11,12];
+// b =a;
+// b.pop();
+// b.push(50);
+
+//              primitive data types-----
+
+// var chahca = "hajdfd";
+// var chahca2 = chahca;
+// let a = 12;
+// const b = 559;
+
+
+//               reference data types-----(koii bhi bracket se bana hua data type object hota hai)
+
+// var a = [1,2,3,4,5,6,7];
+// var b = a;
+
+// let   obj = {
+// name : "harsh",
+// age : 21, };
+
+
+//                conditional statements-----
+
+// if(11<12){
+//     return false;
+//     //console.log("hello");
+
+// }else{
+//     return true;
+//     //console.log("bye");
+// }
+
+
+//                  loops-----
+
+// for( var i =1; i <10; i++){
+//     console.log(i);
+// }
+
+// while(10>5){
+//     console.log("10 is greater than 5");
+//     break;
+// }
+
+// var a = 0;
+// while(a<40){
+//     a++;
+//     console.log(a); 
+// }
+
+
+//             functions-----
+
+// function sum( a,b){
+//     return a+b;
+// }
+
+// var d = 30;
+// var h = 40;
+// var sum1 = sum(d,h);
+// console.log(sum1);
+
+
+// function mul(a,b){
+//     return a*b;
+// }
+
+// let c = 70;
+// let v = 4;
+// console.log(mul(c,v));
+
+
+// function detail(name, age){
+//     console.log("my name is " + name + " and my age is " + age);
+// }
+
+// detail("vaibhaw singh", 21);
+
+
+
+// function chacha(){
+//     console.log("hello chacha");
+// }
+// chacha();
+
+
+
+// function value(a){
+//     console.log(a);
+// }
+
+// value(20);
+// value("harsh singh");
+// value([1,2,3,4,5]);
+// value({name : "harsh", age : 21});
+
+
+//              arrow function-----( arraw fredam deta hai ak se jada parameter lene me, group of values)
+
+
+// var array = [1,2,3,4,5,6,7,8,9,10];
+// console.log(array);
+
+// array.push(11);
+// console.log(array);
+
+// array.pop();
+// console.log(array);
+
+// array.shift();
+// console.log(array);
+
+// array.unshift(0);
+// console.log(array);
+
+// array.splice(3,0,3);
+// console.log(array);
+
+// array.splice(3,1);
+// console.log(array);
+
+// array.splice(2,2);
+// console.log(array);
+
+
+
+//            objects-----( key value pair , ak loge ka bare me sara baat karn  huwa oblect)----
+
+
+// var obj ={
+//     name  :"harsh singh ",
+//     age : 21,
+//      city : "varanasi ",
+//      hobbies : ["playing , sleeping , coding"],
+// }
+
+// obj.age= 22;
+// obj.city = "lucknow";
+// obj.hobbies.push("reading books");
+
+// console.log(obj);
+
+
---
+++ DAY_01/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+function detail(name, age){
+//     console.log("my name is " + name + " and my age is " + age);
+// }
+
+// detail("vaibhaw singh", 21);
+

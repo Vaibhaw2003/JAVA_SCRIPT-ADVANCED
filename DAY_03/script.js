9d01c41955b1a8853b9dfcd262df56d02390f44c
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2025-10-07 01:46:28 +0530

    process

--- DAY_03/script.js
+++ DAY_03/script.js
@@ -1,10 +1,11 @@
-var a = 500;
-var b = 100;
-var c =b/a;
+// var a = 500;
+// var b = 100;
+// var c =b/a;
 
 
-if(a<b){
-    console.log("true");
-}else{
-    console.log(c);
-}
+// if(a<b){
+//     console.log("true");
+// }else{
+//     console.log(c);
+// }
+
---
+++ DAY_04/script.js
@@ -0,0 +1,7 @@
+//--------- variable decleyard-------
+// let a =34;
+// var b =40;
+//const v = 40;
+// var c = 60;
+// var sum = a+b+c+v;
+//  console.log(sum)
